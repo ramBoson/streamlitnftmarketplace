878a7390c437da005d8da9f73cfe66cc9c9b1faa
ramBoson <ramamugesh95@gmail.com>
2021-10-06 12:28:17 +0530

    add initial nft marketplace

---
+++ src/private.py
@@ -0,0 +1,4 @@
+from algosdk import *
+# get account from mnemonic
+private_key = mnemonic.to_private_key('forget onion since true believe pelican calm kiwi brother cool false blanket priority depart plate update match jaguar smoke absorb glimpse buyer oxygen about shaft')
+print(private_key)
